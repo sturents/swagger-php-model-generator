17bcc04d985b5c5dbffd2caf78531e0c8c8eb487
m1ke <git@m1ke.me>
2018-07-25 17:27:13 +0100

    Added array add method and output for example script

--- example.php
+++ example.php
@@ -23,6 +23,12 @@ if (!empty($opt_errors)){
 	throw new Exception("The following option errors were encountered: \n".implode("\n", $opt_errors)."\n");
 }
 
+echo "Generating models under namespace '{$options['namespace']}' from the YAML file at '{$options['yaml-path']}'\n";
+
 $generator = new GenerateModels($options['namespace']);
 $generator->generate($options['yaml-path']);
+
+echo "Saving ".count($generator->classes)." files to {$options['model-dir']}\n";
 $generator->saveClasses($options['model-dir']);
+
+echo "Done\n";
--- src/GenerateModels.php
+++ src/GenerateModels.php
@@ -19,16 +19,25 @@ class GenerateModels {
 	 */
 	public $classes = [];
 
+	/**
+	 * Creates a generator for a specific namespace
+	 *
+	 * @param string $namespace_name
+	 */
 	public function __construct(string $namespace_name){
 		$this->namespace_name = $this->stringNotEndWith($namespace_name, '\\');
 	}
 
+	/**
+	 * Generates classes in the "classes" field
+	 *
+	 * @param string $file_path
+	 */
 	public function generate(string $file_path){
 		$namespace_name = $this->namespace_name;
 
 		$api = Yaml::parseFile($file_path);
 
-		$converter = new CamelCaseToSnakeCaseNameConverter;
 		$namespace = new PhpNamespace($namespace_name);
 
 		foreach ($api['definitions'] as $class_name => $class_details){
@@ -48,51 +57,91 @@ class GenerateModels {
 				$properties = $class_details['properties'];
 			}
 
-			foreach ($properties as $property_name => $property_details){
-				if (isset($property_details['$ref'])){
-					$type = $this->typeFromRef($property_details);
-					$typehint = "$namespace_name\\$type";
-				}
-				else {
-					$type = $property_details['type'];
-					$typehint = $type;
-				}
-
-				/**
-				 * @var Property $property
-				 */
-				$property = $class->addProperty($property_name)->setVisibility('private');
-				$property->addComment($property_details['description'] ?? "\n");
-
-				if ($type==='array'){
-					$comment_type = $this->typeFromRef($property_details['items']);
-					$comment_type = "{$comment_type}[]";
-				}
-				else {
-					$comment_type = $type;
-				}
-
-				$property->addComment("@var $comment_type");
-
-				$capital_case = $converter->denormalize($property_name);#
-				$capital_case = ucfirst($capital_case);
-				$class->addMethod('get'.$capital_case)->setBody("return \$this->$property_name;")->addComment("@return $comment_type");
-				/**
-				 * @var Method $setter
-				 */
-				$setter = $class->addMethod('set'.$capital_case)->setBody("\$this->$property_name = \$$property_name;\n\nreturn \$this;")->addComment("@param $comment_type \$$property_name")->addComment('')->addComment("@return \$this");
-
-				$set_parameter = $setter->addParameter($property_name);
-				if (!in_array($type, ['integer', 'string', 'boolean'])){
-					$set_parameter->setTypeHint($typehint);
-				}
-			}
+			$this->classProperties($properties, $class);
 
 			$this->classes[$class_name] = $class;
 		}
 	}
 
 	/**
+	 * @param array $properties
+	 * @param ClassType $class
+	 */
+	private function classProperties(array $properties, ClassType $class){
+		$converter = new CamelCaseToSnakeCaseNameConverter;
+		$namespace_name = $this->namespace_name;
+
+		foreach ($properties as $property_name => $property_details){
+			if (isset($property_details['$ref'])){
+				$type = $this->typeFromRef($property_details);
+				$typehint = "$namespace_name\\$type";
+			}
+			else {
+				$type = $property_details['type'];
+				$typehint = $type;
+			}
+
+			/**
+			 * @var Property $property
+			 */
+			$property = $class->addProperty($property_name)->setVisibility('private');
+			$property->addComment($property_details['description'] ?? "\n");
+
+			if ($type==='array'){
+				$sub_type = $this->typeFromRef($property_details['items']);
+				$comment_type = "{$sub_type}[]";
+				if (isset($property_details['items']['$ref'])){
+					$sub_typehint = "$namespace_name\\$sub_type";
+				}
+				else {
+					$sub_typehint = $sub_type;
+				}
+			}
+			else {
+				$comment_type = $type;
+				$sub_type  = $sub_typehint = '';
+			}
+
+			$property->addComment("@var $comment_type");
+
+			$capital_case = $converter->denormalize($property_name);#
+			$capital_case = ucfirst($capital_case);
+			$class->addMethod('get'.$capital_case)
+				->setBody("return \$this->$property_name;")
+				->addComment("@return $comment_type");
+			/**
+			 * @var Method $setter
+			 */
+			$setter = $class->addMethod('set'.$capital_case)
+				->setBody("\$this->$property_name = \$$property_name;\n\nreturn \$this;")
+				->addComment("@param $comment_type \$$property_name")
+				->addComment('')
+				->addComment("@return \$this");
+
+			$set_parameter = $setter->addParameter($property_name);
+			if (!in_array($type, ['integer', 'string', 'boolean'])){
+				$set_parameter->setTypeHint($typehint);
+			}
+
+			if ($sub_type){
+				$property_name_singular = $this->stringNotEndWith($property_name, 's');
+				$capital_case_singular = $this->stringNotEndWith($capital_case, 's');
+				/**
+				 * @var Method $add_to
+				 */
+				$add_to = $class->addMethod('add'.$capital_case_singular)->setBody("\$this->{$property_name}[] = \$$property_name_singular;\n\nreturn \$this;")->addComment("@param $sub_type \$$property_name_singular")->addComment('')->addComment("@return \$this");
+
+				$set_parameter = $add_to->addParameter($property_name_singular);
+				if (!in_array($sub_type, ['integer', 'string', 'boolean'])){
+					$set_parameter->setTypeHint($sub_typehint);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Saves generated classes down as PHP files
+	 *
 	 * @param string $dir
 	 * @throws \Exception
 	 */
@@ -119,11 +168,20 @@ class GenerateModels {
 		}
 	}
 
+	/**
+	 * Utility function
+	 *
+	 * @param string $string
+	 * @param string $char
+	 * @return string
+	 */
 	private function stringNotEndWith(string $string, string $char){
 		return $string[strlen($string)-1]===$char ? substr($string, 0, -1): $string;
 	}
 
 	/**
+	 * Changes a Swagger definition into a type
+	 *
 	 * @param array $property
 	 * @return string
 	 */
