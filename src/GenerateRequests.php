98db0f9a63570af2ba62bc06a96c267a099a9080
m1ke <git@m1ke.me>
2018-07-26 14:13:15 +0100

    Refactoring

--- src/GenerateRequests.php
+++ src/GenerateRequests.php
@@ -17,14 +17,12 @@ class GenerateRequests extends ClassGenerator {
 		$this->base_uri = $base_uri;
 	}
 
-
 	/**
 	 * @param string $file_path
 	 */
 	public function generate(string $file_path){
 		$api = Yaml::parseFile($file_path);
 
-		$model_ns = $this->namespaceModel();
 		foreach ($api['paths'] as $path => $path_details){
 			$path_no_params = $this->pathNoParams($path);
 			$path_camel = $this->pathToCamelCase($path_no_params);
@@ -37,60 +35,13 @@ class GenerateRequests extends ClassGenerator {
 				$class->addConstant('URI', "{$this->base_uri}/{$path_no_params}");
 				$class->addConstant('METHOD', strtoupper($method));
 
-				$method_details['parameters'] = $method_details['parameters'] ?: [];
-				$path_params = [];
-				foreach ($method_details['parameters'] as $parameter){
-					switch ($parameter['in']){
-						case 'path':
-							$path_params[] = $parameter;
-						break;
-						case 'body':
-							$schema = $parameter['schema'];
-							if (!empty($schema)){
-								$type = $this->typeFromRef($schema);
-								$name = strtolower($parameter['name']);
-								$model_class = "\\$model_ns\\$type";
-								$class->addMethod('setBody')->addComment("@param $model_class \$$name")->setBody("\$this->body = json_encode(\$$name);")->addParameter($name)->setTypeHint($model_class);
-							}
-						break;
-					}
-				}
+				$path_params = $this->handleParams($method_details, $class);
 
-				if (!empty($path_params)){
-					$constructor = $class->addMethod('__construct');
-					$param_names = [];
-					foreach ($path_params as $path_param){
-						$param_name = $path_param['name'];
-						$param_names[] = $param_name;
-						$constructor->addParameter($param_name);
-						$class->addProperty($param_name)->addComment($path_param['description'])->addComment('')->addComment("@var {$path_param['type']}");
-						$constructor->addBody("\$this->$param_name = \$$param_name;");
-					}
-
-					if (!empty($param_names)){
-						$class->addProperty('param_names')
-							->setStatic(true)
-							->setValue($param_names)
-							->setVisibility('protected');
-					}
-				}
+				$this->handlePathParams($path_params, $class);
 
 				$this->classes[$class_name] = $class;
 			}
 		}
-	}
-
-	public function saveClasses(string $dir){
-		$this->saveClassesInternal($dir, $this->namespaceRequest());
-	}
-
-	/**
-	 * @param string $dir
-	 */
-	public function dumpParentClass(string $dir){
-		$file = __DIR__.'/SwaggerRequest.php';
-		$namespace = $this->namespaceRequest();
-		$this->dumpParentInternal($dir, $file, $namespace);
 	}
 
 	/**
@@ -128,5 +79,92 @@ class GenerateRequests extends ClassGenerator {
 		$path = implode('/', $path_no_params);
 
 		return $path;
+	}
+
+	/**
+	 * @param $path_params
+	 * @param $class
+	 */
+	private function handlePathParams(array $path_params, ClassType $class){
+		if (empty($path_params)){
+			return;
+		}
+
+		$constructor = $class->addMethod('__construct');
+		$param_names = [];
+		foreach ($path_params as $path_param){
+			$param_name = $path_param['name'];
+			$param_names[] = $param_name;
+			$constructor->addParameter($param_name);
+			$class->addProperty($param_name)
+				->addComment($path_param['description'])
+				->addComment('')
+				->addComment("@var {$path_param['type']}");
+			$constructor->addBody("\$this->$param_name = \$$param_name;");
+		}
+
+		if (!empty($param_names)){
+			$class->addProperty('param_names')
+				->setStatic(true)
+				->setValue($param_names)
+				->setVisibility('protected');
+		}
+	}
+
+	/**
+	 * @param array $method_details
+	 * @param ClassType $class
+	 *
+	 * @return array
+	 */
+	protected function handleParams(array $method_details, ClassType $class){
+		$parameters = $method_details['parameters'] ?: [];
+		$path_params = [];
+		foreach ($parameters as $parameter){
+			switch ($parameter['in']){
+				case 'path':
+					$path_params[] = $parameter;
+				break;
+				case 'body':
+					$this->handleBodyParam($parameter, $class);
+				break;
+			}
+		}
+
+		return $path_params;
+	}
+
+	public function saveClasses(string $dir){
+		$this->saveClassesInternal($dir, $this->namespaceRequest());
+	}
+
+	/**
+	 * @param string $dir
+	 */
+	public function dumpParentClass(string $dir){
+		$file = __DIR__.'/SwaggerRequest.php';
+		$namespace = $this->namespaceRequest();
+		$this->dumpParentInternal($dir, $file, $namespace);
+	}
+
+	/**
+	 * @param array $parameter
+	 * @param ClassType $class
+	 */
+	private function handleBodyParam(array $parameter, ClassType $class){
+		$schema = $parameter['schema'];
+		if (empty($schema)){
+			return;
+		}
+
+		$type = $this->typeFromRef($schema);
+		$name = strtolower($parameter['name']);
+		$model_ns = $this->namespaceModel();
+		$model_class = "\\$model_ns\\$type";
+		$class->addMethod('setBody')
+			->addComment("@param $model_class \$$name")
+			->setBody("\$this->body = json_encode(\$$name);")
+			->addParameter($name)
+			->setTypeHint($model_class);
 	}
 }
